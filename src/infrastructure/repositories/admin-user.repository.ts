1cbaf981d8784490036cb79f91066d4999b6ca9a
adrogo.dev <adrogo.dev@gmail.com>
2026-05-06 16:29:31 -0500

    feat(repository): implementar findByUsername y updateAuthToken en AdminUserRepository

--- src/infrastructure/repositories/admin-user.repository.ts
+++ src/infrastructure/repositories/admin-user.repository.ts
@@ -9,4 +9,15 @@ export class AdminUserRepository extends BaseRepository<admin_user, bigint> impl
     constructor(prisma: PrismaService) {
         super(prisma, 'admin_user', 'id_user');
     }
+
+    async findByUsername(username: string): Promise<admin_user | null> {
+        return await this._model.findFirst({ where: { username } });
+    }
+
+    async updateAuthToken(id: bigint, token: string): Promise<void> {
+        await this._model.update({
+            where: { id_user: id },
+            data: { auth_token: token, updated_at: new Date() },
+        });
+    }
 }
