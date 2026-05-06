fcaddeecec43c677323eaae2ea1003f088e97b45
adrogo.dev <adrogo.dev@gmail.com>
2026-05-06 16:30:28 -0500

    chore(app): registrar AdminUserModule en AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './infrastructure/prisma/prisma.module';
 import { RepositoriesModule } from './infrastructure/repositories/repositories.module';
+import { AdminUserModule } from './presentation/controllers/admin-user/admin-user.module';
 
 @Module({
-    imports: [PrismaModule, RepositoriesModule],
+    imports: [PrismaModule, RepositoriesModule, AdminUserModule],
 })
 export class AppModule {}
 
