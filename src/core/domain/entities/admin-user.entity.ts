41c437dce95757f0d98703ae7f12939508295cef
adrogo.dev <adrogo.dev@gmail.com>
2026-05-05 21:48:43 -0500

    feat(entity): agregar entidad AdmiUser y контракт ADMIN_USER_REPOSITORY
    - Crear entidad AdmiUser con campos del modelo admin_user
    - Crear contrato IAdminUserRepository extends IBaseRepository
    - Crear script seed.ts para datos iniciales de admin_user
    - Crear contrato IBaseRepository con operaciones CRUD base

---
+++ src/core/domain/entities/admin-user.entity.ts
@@ -0,0 +1,15 @@
+export class AdmiUser {
+    constructor(
+        readonly id?: bigint,
+        readonly name?: string,
+        readonly surname?: string,
+        readonly username?: string,
+        readonly password?: string,
+        readonly ipConnection?: string,
+        readonly authToken?: string,
+        readonly recoveryTokenData?: string,
+        readonly isActive?: boolean,
+        readonly createdAt?: Date,
+        readonly updatedAt?: Date,
+    ) {}
+}
