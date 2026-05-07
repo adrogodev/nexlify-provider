9bf3407794c38da728e56786c245d5ef40ee5b1b
andres robles <andres.robles@silvertec.com.co>
2026-05-07 12:19:16 -0500

    feat(app): registrar ClientModule en AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './infrastructure/prisma/prisma.module';
 import { RepositoriesModule } from './infrastructure/repositories/repositories.module';
 import { AdminUserModule } from './presentation/controllers/admin-user/admin-user.module';
+import { ClientModule } from './presentation/controllers/client/client.module';
 
 @Module({
-    imports: [PrismaModule, RepositoriesModule, AdminUserModule],
+    imports: [
+        PrismaModule,
+        RepositoriesModule,
+        AdminUserModule,
+        ClientModule
+    ],
 })
-export class AppModule {}
+export class AppModule { }
 
