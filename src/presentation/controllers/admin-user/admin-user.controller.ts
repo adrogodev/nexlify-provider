3b48f7f7c307638d6a944c04f6f103cc17084ecc
andres robles <andres.robles@silvertec.com.co>
2026-05-07 09:21:19 -0500

    refactor(controller): inyectar IP del request y adaptar endpoint de autenticación al AuthDto

--- src/presentation/controllers/admin-user/admin-user.controller.ts
+++ src/presentation/controllers/admin-user/admin-user.controller.ts
@@ -1,18 +1,23 @@
-import { Body, Controller, HttpCode, Post } from '@nestjs/common';
-import { AuthAdminUserRequestData } from 'src/core/application/use-cases/admin-user/auth/auth-admin-user.request-data';
-import { AuthAdminUserUseCase } from 'src/core/application/use-cases/admin-user/auth/auth-admin-user.usecase';
-import { UnauthenticatedException } from 'src/core/domain/exceptions';
-import { ClientResponse } from 'src/core/domain/models/client-response.model';
+import { Body, Controller, HttpCode, Ip, Post } from '@nestjs/common';
+import { AuthDto } from 'src/core/application/dtos';
+import { AuthAdminUserRequestData, AuthAdminUserUseCase } from 'src/core/application/use-cases/admin-user/auth';
+import { ClientResponse } from 'src/core/domain/models';
 
-@Controller('admin-user')
+@Controller('api/admin-user')
 export class AdminUserController {
-    constructor(private readonly _authUseCase: AuthAdminUserUseCase) {}
+    constructor(private readonly _authUseCase: AuthAdminUserUseCase) { }
 
     @Post('auth')
     @HttpCode(200)
-    async auth(@Body() data: AuthAdminUserRequestData): Promise<ClientResponse<{ token: string }>> {
-        const token = await this._authUseCase.run({ data });
-        if (!token) throw new UnauthenticatedException();
-        return new ClientResponse({ ok: true, message: 'Autenticación exitosa', data: { token } });
+    async auth(@Ip() ip: string, @Body() body: AuthAdminUserRequestData): Promise<ClientResponse<AuthDto>> {
+
+        const ip_connection = (ip ?? '').replace('::ffff:', '');
+        const data = await this._authUseCase.run({ data: { ...body, ip_connection } });
+
+        return new ClientResponse({
+            ok: true,
+            message: 'Inicio de sesion exitoso',
+            data
+        });
     }
 }
