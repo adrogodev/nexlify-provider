8303a4bd8851a0cc57f73cc79e7d7ca59d5913a8
andres robles <andres.robles@silvertec.com.co>
2026-05-07 09:21:26 -0500

    fix(module): agregar inyección de ENCRYPTER en factory de AuthAdminUserUseCase

--- src/presentation/controllers/admin-user/admin-user.module.ts
+++ src/presentation/controllers/admin-user/admin-user.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { RepositoriesModule } from 'src/infrastructure/repositories/repositories.module';
 import { HelpersModule } from 'src/infrastructure/helpers/helpers.module';
 import { ADMIN_USER_REPOSITORY } from 'src/core/application/contracts/persistence';
-import { HASH_GENERATOR, JWT_GENERATOR } from 'src/core/application/contracts/infrastructure';
+import { ENCRYPTER, HASH_GENERATOR, JWT_GENERATOR } from 'src/core/application/contracts/infrastructure';
 import { AuthAdminUserUseCase } from 'src/core/application/use-cases/admin-user/auth/auth-admin-user.usecase';
 import { AdminUserController } from './admin-user.controller';
 
@@ -12,9 +12,9 @@ import { AdminUserController } from './admin-user.controller';
     providers: [
         {
             provide: AuthAdminUserUseCase,
-            useFactory: (repo, hashGen, jwtGen) =>
-                new AuthAdminUserUseCase(repo, hashGen, jwtGen),
-            inject: [ADMIN_USER_REPOSITORY, HASH_GENERATOR, JWT_GENERATOR],
+            useFactory: (repo, hashGen, jwtGen, encrypterGen) =>
+                new AuthAdminUserUseCase(repo, hashGen, jwtGen, encrypterGen),
+            inject: [ADMIN_USER_REPOSITORY, HASH_GENERATOR, JWT_GENERATOR, ENCRYPTER],
         },
     ],
 })
