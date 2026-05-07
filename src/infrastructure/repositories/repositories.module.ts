2ce57961b04f31a098539160d5cb4a7478aaa5a9
andres robles <andres.robles@silvertec.com.co>
2026-05-07 14:48:24 -0500

    feat(repositories): registrar SmtpServerRepository en RepositoriesModule

--- src/infrastructure/repositories/repositories.module.ts
+++ src/infrastructure/repositories/repositories.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ADMIN_USER_REPOSITORY, CLIENT_REPOSITORY } from 'src/core/application/contracts/persistence';
+import { SMTP_SERVER_REPOSITORY } from 'src/core/application/contracts/persistence/smtp-server.repository';
 import { PrismaModule } from '../prisma/prisma.module';
 import { AdminUserRepository } from './admin-user.repository';
-import { ADMIN_USER_REPOSITORY, CLIENT_REPOSITORY } from 'src/core/application/contracts/persistence';
 import { ClientRepository } from './client.repository';
+import { SmtpServerRepository } from './smtp-server.repository';
 
 @Module({
     imports: [PrismaModule],
@@ -14,8 +16,12 @@ import { ClientRepository } from './client.repository';
         {
             provide: CLIENT_REPOSITORY,
             useClass: ClientRepository
+        },
+        {
+            provide: SMTP_SERVER_REPOSITORY,
+            useClass: SmtpServerRepository
         }
     ],
-    exports: [ADMIN_USER_REPOSITORY, CLIENT_REPOSITORY],
+    exports: [ADMIN_USER_REPOSITORY, CLIENT_REPOSITORY, SMTP_SERVER_REPOSITORY],
 })
 export class RepositoriesModule { }
