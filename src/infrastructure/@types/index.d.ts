7c7d6051c6b684cf916c3c14e2c4e939c733b150
andres robles <andres.robles@silvertec.com.co>
2026-05-07 12:18:54 -0500

    feat(types): extender interfaz Request de Express con propiedad auth_data

--- src/infrastructure/@types/index.d.ts
+++ src/infrastructure/@types/index.d.ts
@@ -1,5 +1,13 @@
 export type { }
 
+import type { TokenInfo } from 'src/core/domain/models';
+
 declare global {
     type Nullable<T> = T | null;
+
+    namespace Express {
+        interface Request {
+            auth_data: TokenInfo;
+        }
+    }
 }
