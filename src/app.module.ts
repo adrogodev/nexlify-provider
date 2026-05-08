4bd54125f93d729fae8b4b1cc83127317ae33506
andres robles <andres.robles@silvertec.com.co>
2026-05-08 15:35:21 -0500

    chore(app): registrar ServicesModule en AppModule
    - Se importa `ServicesModule` en el módulo raíz para disponibilizar los servicios de infraestructura

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,9 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './infrastructure/prisma/prisma.module';
 import { RepositoriesModule } from './infrastructure/repositories/repositories.module';
 import { ControllerModule } from './presentation/controllers/controller.module';
+import { ServicesModule } from './infrastructure/services/service.module';
 
 @Module({
-    imports: [PrismaModule, RepositoriesModule, ControllerModule],
+    imports: [PrismaModule, RepositoriesModule, ControllerModule, ServicesModule],
 })
 export class AppModule { }
-
