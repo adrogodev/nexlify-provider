f8cca2c48c2372b38cb01596e49402292d15ee4b
andres robles <andres.robles@silvertec.com.co>
2026-05-11 10:57:10 -0500

    feat(dtos): agregar NexlifyConfigurationDto

---
+++ src/core/application/dtos/nexlify-configuration.dto.ts
@@ -0,0 +1,42 @@
+export interface NexlifyConfigurationDto {
+    sender: SenderData,
+    smtp: SMTPData,
+    limits: LimitData,
+    template: EmailTemplate
+}
+
+interface SenderData {
+    sender_email: string,
+    sender_name: string;
+    reply_to_email: Nullable<string>;
+    mail_application_password: string
+}
+
+interface SMTPData {
+    provider: string;
+    name: string,
+    port: number
+}
+
+interface LimitData {
+    daily_send_limit: number,
+    max_retries: number,
+    retry_interval_seconds: number
+}
+
+interface EmailTemplate {
+    header: Nullable<HeaderData>,
+    footer: Nullable<FooterData>
+}
+
+interface HeaderData {
+    email_header_text?: Nullable<string>;
+    email_header_html?: Nullable<string>;
+    email_header_img_url?: Nullable<string>;
+}
+
+interface FooterData {
+    email_footer_text?: Nullable<string>;
+    email_footer_html?: Nullable<string>;
+    email_footer_img_url?: Nullable<string>;
+}
