e65211c1f098d22302949345847c4b8a0ead9d9e
andres robles <andres.robles@silvertec.com.co>
2026-05-27 15:30:27 -0500

    feat(repository): implementar verifyIfExist y getAllClientByFilters
    - Implementar verifyIfExist usando count con filtro por id_number
    - Implementar getAllClientByFilters con where dinámico, paginación y total de registros sin filtros

--- src/infrastructure/repositories/client.repository.ts
+++ src/infrastructure/repositories/client.repository.ts
@@ -9,4 +9,27 @@ export class ClientRepository extends BaseRepository<Client, 'id_client'> implem
     constructor(prisma: PrismaService) {
         super(prisma, 'clients', 'id_client');
     }
+
+    public async verifyIfExist(id_number: string): Promise<boolean> {
+        return await this._model.count({ where: { id_number } }) > 0;
+
+    }
+
+    public async getAllClientByFilters(page: number, size: number, id_state: number, id_type: number, id_number: string, name: string): Promise<{ count: number; data: Nullable<Client[]>; }> {
+        const where: Record<string, any> = {};
+
+        if (id_state) where.id_state = id_state;
+        if (id_type) where.id_type = id_type;
+        if (id_number) where.id_number = id_number;
+
+        if (name) where.name = { contains: name, mode: 'insensitive' };
+
+        const skip = (page - 1) * size;
+
+        const total_items = await this._model.count();
+        const clients = await this._model.findMany({ where, take: size, skip, orderBy: { id_client: "desc" } });
+
+        return { count: total_items, data: clients }
+    }
+
 }
