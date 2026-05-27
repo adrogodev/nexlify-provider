6141fd8f6b39a0843d6c9403110a3918047a211a
andres robles <andres.robles@silvertec.com.co>
2026-05-27 15:31:42 -0500

    feat(module): registrar nexlify-keys repositories en el módulo
    - Agregar providers para NexlifyKeysRepository y NexlifyKeysLogsRepository
    - Exportar NEXLIFY_KEYS_REPOSITORY y NEXLIFY_KEYS_LOGS_REPOSITORY

--- src/infrastructure/repositories/repositories.module.ts
+++ src/infrastructure/repositories/repositories.module.ts
@@ -4,8 +4,10 @@ import { PrismaModule } from '../prisma/prisma.module';
 import { ClientRepository } from './client.repository';
 import { NexlifyConfigurationRepository } from './nexlify-configuration.repository';
 import { SmtpServerRepository } from './smtp-server.repository';
-import { ADMIN_USER_REPOSITORY, CLIENT_CREDENTIALS_REPOSITORY, CLIENT_REPOSITORY, NEXLIFY_CONFIGURATION_REPOSITORY, SMTP_SERVER_REPOSITORY } from 'src/core/application/contracts/persistence';
+import { ADMIN_USER_REPOSITORY, CLIENT_CREDENTIALS_REPOSITORY, CLIENT_REPOSITORY, NEXLIFY_CONFIGURATION_REPOSITORY, NEXLIFY_KEYS_LOGS_REPOSITORY, NEXLIFY_KEYS_REPOSITORY, SMTP_SERVER_REPOSITORY } from 'src/core/application/contracts/persistence';
 import { ClientCredentialsRepository } from './client_credentials.repository';
+import { NexlifyKeysRepository } from './nexlify-keys.repository';
+import { NexlifyKeysLogsRepository } from './nexlify-keys-logs.repository';
 
 @Module({
     imports: [PrismaModule],
@@ -30,13 +32,23 @@ import { ClientCredentialsRepository } from './client_credentials.repository';
             provide: CLIENT_CREDENTIALS_REPOSITORY,
             useClass: ClientCredentialsRepository
         },
+        {
+            provide: NEXLIFY_KEYS_REPOSITORY,
+            useClass: NexlifyKeysRepository
+        },
+        {
+            provide: NEXLIFY_KEYS_LOGS_REPOSITORY,
+            useClass: NexlifyKeysLogsRepository
+        },
     ],
     exports: [
         ADMIN_USER_REPOSITORY,
         CLIENT_REPOSITORY,
         SMTP_SERVER_REPOSITORY,
         NEXLIFY_CONFIGURATION_REPOSITORY,
-        CLIENT_CREDENTIALS_REPOSITORY
+        CLIENT_CREDENTIALS_REPOSITORY,
+        NEXLIFY_KEYS_REPOSITORY,
+        NEXLIFY_KEYS_LOGS_REPOSITORY
     ],
 })
 
