1726526cf60db7ecea939bfefc8205f96e3364a3
andres robles <andres.robles@silvertec.com.co>
2026-05-27 10:30:25 -0500

    refactor(db): renombrar assign_credentials a checked_credential_assignment_token

--- src/core/domain/entities/client_credentials.entity.ts
+++ src/core/domain/entities/client_credentials.entity.ts
@@ -6,10 +6,10 @@ export class ClientCredentials {
         public password: Nullable<string>,
         public ip_connection: Nullable<string>,
         public assign_credentials_token_jti: Nullable<string>,
+        public checked_credential_assignment_token: Nullable<boolean>,
         public auth_token: Nullable<string>,
         public recovery_token: Nullable<string>,
         public created_at: Nullable<Date>,
         public updated_at: Nullable<Date>,
-        public assign_credentials: Nullable<boolean>
     ) { }
 }
